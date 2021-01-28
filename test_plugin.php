96169f09dad3261e89c203dc140d82fe00958d5d
nehalayub <nehalleo1896@gmail.com>
2021-01-28 09:27:40 +0000

    main plugin file added

--- test_plugin.php
+++ test_plugin.php
@@ -1,11 +1,10 @@
 <?php
-  
-  echo "working";
 
-  echo "updated";
-
-  echo "updated 2 ";
-
-  echo "updated 3 ";
-
-?>
+/*
+  Plugin Name: Test Plugin
+  Description: Creates an appointment scheduling calendar and allow admin to add multiple boradband plans, packages and services.
+  Version: 1.1.1.0
+  Author: Codup
+  Author URI: http://codup.io/
+  Text Domain: airebeam-scheduler-calendar
+ */
